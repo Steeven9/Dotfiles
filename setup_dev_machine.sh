a23330a6cff830cc9e9d42a165cf06594b807671
Stefano Taillefert <stefano.taille@gmail.com>
2023-06-01 14:29:51 +0000

    Fixes

--- setup_dev_machine.sh
+++ setup_dev_machine.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-brew installmaven gradle node@16 php thefuck yarn \
-    mongosh openjdk@8 openjdk@17 speedtest-cli tokei \
+brew install maven gradle node@16 php thefuck yarn \
+    mongosh openjdk@17 speedtest-cli tokei \
     htop python@3.11 docker docker-compose vsce texlive latexindent
 
 brew install go helm podman k9s kubectl minikube istioctl skaffold \
     docker-credential-helper openfortivpn mimirtool jq cloudfoundry/tap/cf-cli@8
 
 # casks
-brew install openvpn-connect firefox iterm2 gimp spotify vlc thunderbird nextcloud
-
-# powerline fonts
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
+brew install openvpn-connect firefox discord jellyfin-media-player \
+    iterm2 gimp spotify vlc thunderbird nextcloud
 
 # gpg keys
 # gpg --import {PRIVATE_KEY}
